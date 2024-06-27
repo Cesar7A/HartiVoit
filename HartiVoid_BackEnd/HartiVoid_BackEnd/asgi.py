add5d437b42d7dd7ab7e19c0a90494b486bf9b79
Marbin Javier Arévalo Guerrero <112355950+noNameMJ@users.noreply.github.com>
2024-06-27 10:55:52 -0500

    Change to Restframework

--- HartiVoid_BackEnd/HartiVoid_BackEnd/asgi.py
+++ HartiVoid_BackEnd/HartiVoid_BackEnd/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for HartiVoit project.
+ASGI config for HartiVoid_BackEnd project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HartiVoit.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'HartiVoid_BackEnd.settings')
 
 application = get_asgi_application()
--- HartiVoid_BackEnd/HartiVoid_BackEnd/urls.py
+++ HartiVoid_BackEnd/HartiVoid_BackEnd/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for HartiVoit project.
+URL configuration for HartiVoid_BackEnd project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
