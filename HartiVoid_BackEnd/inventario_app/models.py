80a6e9b6e9aca1b57d3bed68204fec589d7b2905
Marbin Javier Arévalo Guerrero <112355950+noNameMJ@users.noreply.github.com>
2024-06-28 03:15:10 -0500

    Cambios a los modelos

--- HartiVoid_BackEnd/inventario_app/models.py
+++ HartiVoid_BackEnd/inventario_app/models.py
@@ -20,8 +20,18 @@ class Producto(models.Model):
     marca = models.CharField(max_length=100)
     linea = models.ForeignKey(Linea, on_delete=models.CASCADE)
     proveedor = models.ForeignKey(Proveedor, on_delete=models.CASCADE)
+    descripcion = models.CharField(max_length=350,default='Descripción')
     precio_detal = models.DecimalField(max_digits=20, decimal_places=2)
     precio_mayor = models.DecimalField(max_digits=20, decimal_places=2)
 
+    custom_id = models.CharField(max_length=50, unique=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.custom_id:
+            self.custom_id = self.generate_custom_id()
+        super(Producto, self).save(*args, **kwargs)
+
+    def generate_custom_id(self):
+        return f"{self.linea.id}-{self.proveedor.id}-{self.id}"
     def __str__(self):
         return self.nombre
