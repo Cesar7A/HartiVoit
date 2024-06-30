28096d8e1edabc093d3fab11c69fa0aa61f5efe1
Marbin Javier Arévalo Guerrero <112355950+noNameMJ@users.noreply.github.com>
2024-06-30 12:27:18 -0500

    Custom id como PLU

--- HartiVoid_BackEnd/inventario_app/models.py
+++ HartiVoid_BackEnd/inventario_app/models.py
@@ -23,15 +23,18 @@ class Producto(models.Model):
     descripcion = models.CharField(max_length=350,default='Descripción')
     precio_detal = models.DecimalField(max_digits=20, decimal_places=2)
     precio_mayor = models.DecimalField(max_digits=20, decimal_places=2)
-
-    custom_id = models.CharField(max_length=50, unique=True, blank=True)
+    custom_id = models.CharField(max_length=255, unique=True, blank=True)
 
     def save(self, *args, **kwargs):
+        # Guarda el objeto para asegurarse de que tiene un ID
+        if not self.id:
+            super(Producto, self).save(*args, **kwargs)
+        
+        # Genera el custom_id
         if not self.custom_id:
-            self.custom_id = self.generate_custom_id()
+            self.custom_id = f"{self.linea.id:02}{self.proveedor.id:02}{self.id:02}"
+        
         super(Producto, self).save(*args, **kwargs)
-
-    def generate_custom_id(self):
-        return f"{self.linea.id}-{self.proveedor.id}-{self.id}"
+        
     def __str__(self):
         return self.nombre
